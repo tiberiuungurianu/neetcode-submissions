7f30a7af7c205ca7d75b9b999c145c2c667034dc
tiberiuungurianu <92324038+tiberiuungurianu@users.noreply.github.com>
2026-07-21 19:15:42 +0100

    Add: duplicate-integer - submission-5

---
+++ Data Structures & Algorithms/duplicate-integer/submission-5.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        int [] fq = new int[nums.length]
+        while ( i < nums.length)
+        {
+            if (f)
+        }
+    //     boolean found = false;
+    //     int n =  nums.length;
+    //     int i=1;
+    //     while (n!=0 && i<nums.length)
+    //     {
+    //         if( nums[i-1]==nums[i])
+    //         {
+    //             found = true;
+    //             break;
+    //         }
+    //         else i++;
+    //     }
+    //     return found;
+    // }
+    }
+}
